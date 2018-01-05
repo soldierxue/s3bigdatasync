309df088fe6ac8dc1c395f5a2e1956f19cb48e41
Leo Chen <chenlt78@qq.com>
2018-01-05 10:57:29 +0800

    Adding tasks to sqs. Testing done
    Signed-off-by: Leo Chen <chenlt78@qq.com>

--- ListProducer/ListProducer.py
+++ ListProducer/ListProducer.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python
 # -*- coding: utf8 -*-
 
-
-INVENTORY_BUCKET="leo-bjs-inventory-bucket"
-INVENTORY_KEY_DIR="leodatacenter/leodatacenter/2017-12-30T08-00Z/"
-
-
 from pprint import pprint
 from s3_utils import *
+from sqs_utils import *
+import random
 
-
+# 0. Job
+job_info = {
+    'src_type': 's3_inventory',
+    'inventory_bucket': 'leo-bjs-inventory-bucket',
+    'inventory_manifest_dir': 'leodatacenter/leodatacenter/2017-12-30T08-00Z/',
+    'queue_url_prefix': 'https://sqs.cn-north-1.amazonaws.com.cn/358620020600/s3sync-worker',
+    'queue_num': 1, # How many SQS to use
+    'message_body_max_num': 100, # How many objects in one message body
+    'dst_bucket': 'leo-zhy-datacenter'
+}
 
 # Inventory process
 
-
-def parse_inventory_data_file(data_file):
-
+def parse_inventory_data_file(data_file, job_info=None):
     msg_body=[]
+
+    number=0
 
     with gzip.open(data_file, 'rb') as f:
         for line in f.readlines():
+            number+=1
             sections = line.split(',')
 
             if len(sections) < 7:
@@ -29,33 +36,32 @@ def parse_inventory_data_file(data_file):
             key = sections[1].split('"')[1]
 
             #print src_bucket,key,dst_bucket
-            print src_bucket,key
+            #print src_bucket,key
 
-            #msg_body.append({'src_bucket':src_bucket, 'key':key, 'dst_bucket':dst_bucket})
+            msg_body.append({'src_bucket':src_bucket, 'key':key, 'dst_bucket':job_info['dst_bucket']})
 
-
-    '''
-            if len(msg_body) == MESSAGE_BODY_NUM:
-                qurl='{0}-{1}'.format(QUEUE_ENDPOINT, random.randint(1, QUEUE_NUM))
+            if len(msg_body) == job_info['message_body_max_num']:
+                qurl='%s-%03d'%(job_info['queue_url_prefix'], random.randint(1, job_info['queue_num']))
                 send_msg_to_sqs(qurl, msg_body)
                 msg_body=[]
 
     if len(msg_body) > 0:
-        qurl='{0}-{1}'.format(QUEUE_ENDPOINT, random.randint(1, QUEUE_NUM))
+        qurl='%s-%03d'%(job_info['queue_url_prefix'], random.randint(1, job_info['queue_num']))
         send_msg_to_sqs(qurl, msg_body)
-    '''
+
+    stat = {
+        'total_number': number
+    }
+    return stat 
 
 def downlad_bucket_manifest():
     ''' test FIXME '''
     # leo-bjs-inventory-bucket', 'leodatacenter/leodatacenter/2017-12-25T08-00Z/
-
-    data = load_json_from_s3_object(INVENTORY_BUCKET, INVENTORY_KEY_DIR+'manifest.json')
+    data = load_json_from_s3_object(job_info['inventory_bucket'], job_info['inventory_manifest_dir']+'manifest.json')
     #print(data)
     return data
 
 def main():
-    print("main")
-
     # 1. Get Source information
     manifest = downlad_bucket_manifest()
 
@@ -63,9 +69,10 @@ def main():
     if 'files' in manifest:
         for item in manifest['files']:
             pprint(item)
-            download_filename = download_s3_object_from_inventory(INVENTORY_BUCKET, item)
+            download_filename = download_s3_object_from_inventory(job_info['inventory_bucket'], item)
 
-            parse_inventory_data_file(download_filename)
+            stat = parse_inventory_data_file(download_filename, job_info)
+            print(stat)
             
 if __name__ == '__main__':
     main()
