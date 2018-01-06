7e29ff1d2fa4a7c66db58e104a6b9995ed54ef3d
Leo Chen <chenlt78@qq.com>
2018-01-06 17:12:22 +0800

    Adding stat; Saving back to inventory dir as job.json
    Signed-off-by: Leo Chen <chenlt78@qq.com>

--- ListProducer/ListProducer.py
+++ ListProducer/ListProducer.py
@@ -14,19 +14,23 @@ job_info = {
     'queue_url_prefix': 'https://sqs.cn-north-1.amazonaws.com.cn/358620020600/s3sync-worker',
     'queue_num': 1, # How many SQS to use
     'message_body_max_num': 100, # How many objects in one message body
+    'src_bucket': 'leodatacenter',
     'dst_bucket': 'leo-zhy-datacenter'
 }
 
+object_key_list= ['totalObjects', 'totalObjectsSub1GB', 'totalObjectsSub5GB', 'totalObjectsSub10GB', 'totalObjectsSub50GB', 'totalObjectsSub100GB', 'totalObjectsSub1TB', 'totalObjectsSub5TB']
 # Inventory process
 
 def parse_inventory_data_file(data_file, job_info=None):
     msg_body=[]
 
-    number=0
+    stat={}
+
+    for key in object_key_list:
+        stat[key]=0
 
     with gzip.open(data_file, 'rb') as f:
         for line in f.readlines():
-            number+=1
             sections = line.split(',')
 
             if len(sections) < 7:
@@ -34,11 +38,70 @@ def parse_inventory_data_file(data_file, job_info=None):
 
             src_bucket = sections[0].split('"')[1]
             key = sections[1].split('"')[1]
+            size = int(sections[2].split('"')[1])
+
+            item = {
+                'Bucket': src_bucket,
+                'Key'   : key,
+                'Size'  : size,
+                'LastModifiedDate': sections[3].split('"')[1],
+                'ETag': sections[4].split('"')[1],
+                'StorageClass': sections[5].split('"')[1],
+                'IsMultipartUploaded': sections[6].split('"')[1],
+                'ReplicationStatus': sections[7].split('"')[1],
+                'dst_bucket':job_info['dst_bucket']
+            }
+
+            # Do Stat
+            stat['totalObjects'] += 1
+
+            if size > 5*1000*1000*1000:
+                pass
+                print(">5T...s3://%s/%s"%(src_bucket, key))
+            elif size > 1000*1000*1000:
+                stat['totalObjectsSub5TB'] += 1
+            elif size > 100*1000*1000:
+                stat['totalObjectsSub1TB'] += 1
+                stat['totalObjectsSub5TB'] += 1
+            elif size > 50*1000*1000:
+                stat['totalObjectsSub100GB'] += 1
+                stat['totalObjectsSub1TB'] += 1
+                stat['totalObjectsSub5TB'] += 1
+            elif size > 10*1000*1000:
+                stat['totalObjectsSub50GB'] += 1
+                stat['totalObjectsSub100GB'] += 1
+                stat['totalObjectsSub1TB'] += 1
+                stat['totalObjectsSub5TB'] += 1
+            elif size > 5*1000*1000:
+                stat['totalObjectsSub10GB'] += 1
+                stat['totalObjectsSub50GB'] += 1
+                stat['totalObjectsSub100GB'] += 1
+                stat['totalObjectsSub1TB'] += 1
+                stat['totalObjectsSub5TB'] += 1
+            elif size > 1000*1000:
+                stat['totalObjectsSub5GB'] += 1
+                stat['totalObjectsSub10GB'] += 1
+                stat['totalObjectsSub50GB'] += 1
+                stat['totalObjectsSub100GB'] += 1
+                stat['totalObjectsSub1TB'] += 1
+                stat['totalObjectsSub5TB'] += 1
+            else:
+                stat['totalObjectsSub1GB'] += 1
+                stat['totalObjectsSub5GB'] += 1
+                stat['totalObjectsSub10GB'] += 1
+                stat['totalObjectsSub50GB'] += 1
+                stat['totalObjectsSub100GB'] += 1
+                stat['totalObjectsSub1TB'] += 1
+                stat['totalObjectsSub5TB'] += 1
+
+            # Bucket, Key, Size, LastModifiedDate, ETag, StorageClass, IsMultipartUploaded, ReplicationStatus
+            # "leodatacenter","AWS+SKO+2015/2015+AWS+Sales+Kickoff+Agenda.pdf","360461","2015-08-29T06:56:01.000Z","eef4ce1bc8503f5ee6c98553ccb9f496","STANDARD","false",""
 
             #print src_bucket,key,dst_bucket
             #print src_bucket,key
 
-            msg_body.append({'src_bucket':src_bucket, 'key':key, 'dst_bucket':job_info['dst_bucket']})
+            #msg_body.append({'src_bucket':src_bucket, 'key':key, 'dst_bucket':job_info['dst_bucket']})
+            msg_body.append(item)
 
             if len(msg_body) == job_info['message_body_max_num']:
                 qurl='%s-%03d'%(job_info['queue_url_prefix'], random.randint(1, job_info['queue_num']))
@@ -49,9 +112,8 @@ def parse_inventory_data_file(data_file, job_info=None):
         qurl='%s-%03d'%(job_info['queue_url_prefix'], random.randint(1, job_info['queue_num']))
         send_msg_to_sqs(qurl, msg_body)
 
-    stat = {
-        'total_number': number
-    }
+    pprint(stat)
+
     return stat 
 
 def downlad_bucket_manifest():
@@ -65,6 +127,10 @@ def main():
     # 1. Get Source information
     manifest = downlad_bucket_manifest()
 
+    manifest['statistics'] = {}
+    for key in object_key_list:
+        manifest['statistics'][key]=0
+
     #pprint(manifest)
     if 'files' in manifest:
         for item in manifest['files']:
@@ -72,7 +138,23 @@ def main():
             download_filename = download_s3_object_from_inventory(job_info['inventory_bucket'], item)
 
             stat = parse_inventory_data_file(download_filename, job_info)
-            print(stat)
-            
+            #print(stat)
+
+            for key in object_key_list:
+                manifest['statistics'][key] += stat[key]
+
+    print("============")
+    manifest['job_info'] = job_info
+
+    pprint(manifest)
+
+    #save back manifest 
+    save_json_to_s3_object(manifest, job_info['inventory_bucket'], job_info['inventory_manifest_dir']+'job.json')
+
+    data = load_json_from_s3_object(job_info['inventory_bucket'], job_info['inventory_manifest_dir']+'job.json')
+
+    pprint(data)
+
+
 if __name__ == '__main__':
     main()
